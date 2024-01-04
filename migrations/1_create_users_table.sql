4ab427df332be14ed5a66ca3becdf5c507848691
Nicolas Auler <nicolasauler@usp.br>
2024-01-04 13:13:32 -0300

    Remove user admin mock.

--- migrations/1_create_users_table.sql
+++ migrations/1_create_users_table.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS users (
     password text NOT NULL
 );
 
-INSERT INTO users (username, password)
-VALUES ('admin', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw');
+--INSERT INTO users (username, password)
+--VALUES ('admin', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw');
