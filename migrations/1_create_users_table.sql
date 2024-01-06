f5e71e4fb1b25f4bb96d59f2d95569cbe035cc25
Nicolas Auler <nicolasauler@usp.br>
2024-01-06 12:26:37 -0300

    Remove user mock from migration.

--- migrations/1_create_users_table.sql
+++ migrations/1_create_users_table.sql
@@ -6,6 +6,3 @@ CREATE TABLE IF NOT EXISTS users (
     email text NOT NULL UNIQUE,
     password text NOT NULL
 );
-
---INSERT INTO users (username, password)
---VALUES ('admin', '$argon2id$v=19$m=19456,t=2,p=1$VE0e3g7DalWHgDwou3nuRA$uC6TER156UQpk0lNQ5+jHM0l5poVjPA1he/Tyn9J4Zw');
