e175f73522f600a1e1fe4d7b9a7ce2490c00bf02
Nicolas Auler <nicolasauler@usp.br>
2024-01-16 00:42:45 -0300

    In axum-extra v0.7.4 html was added as a response type, so where it was possible impl IntoResponse was changed to a simple Html, and maybe we can in the future further optimize the axum responses.

---
+++ src/hypermedia/schema/validation.rs
@@ -0,0 +1,41 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use serde::Deserialize;
+use validator::{Validate, ValidationError};
+use zxcvbn::zxcvbn;
+
+lazy_static! {
+    static ref RE_USERNAME: Regex = Regex::new(r"^[a-z0-9]{3,20}$").unwrap();
+}
+
+#[derive(Deserialize, Validate, Debug)]
+pub struct EmailInput {
+    #[validate(email)]
+    pub email: String,
+}
+
+#[derive(Deserialize, Validate)]
+pub struct UsernameInput {
+    #[validate(regex = "RE_USERNAME")]
+    pub username: String,
+}
+
+#[derive(Deserialize, Validate)]
+pub struct PasswordInput {
+    #[validate(custom = "validate_password_strength")]
+    pub password: String,
+}
+
+fn validate_password_strength(password: &str) -> Result<(), ValidationError> {
+    if zxcvbn(password, &[]).unwrap().score() < 3 {
+        return Err(ValidationError::new("Password is too weak"));
+    }
+    Ok(())
+}
+
+#[derive(Deserialize, Validate)]
+pub struct PasswordsInput {
+    pub password: String,
+    #[validate(must_match = "password")]
+    confirm_password: String,
+}
