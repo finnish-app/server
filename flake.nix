dc059534ccdf1f7f5d15ad185c7cc06e48b1a179
Nicolas Auler <nicolasauler@usp.br>
2024-05-10 01:32:22 -0300

    adding nix flake to develop project

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        shuttle = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "shuttle";
+          version = "v0.44.0";
+
+          src = pkgs.fetchFromGitHub {
+              owner = "shuttle-hq";
+              repo = pname;
+              rev = version;
+              hash = "sha256-3u2GWgDQpa4sU/66vS6S+JwCEL/fvy8MTsATRs7RGVs=";
+          };
+
+          doCheck = false;
+
+          cargoLock = {
+             lockFile = "${src}/Cargo.lock";
+             outputHashes = {
+             "async-posthog-0.2.3" = "sha256-V0f9+UKZkqh80p7UjINEbAW9y8cKBmJTRjAJZV3no1M=";
+             "hyper-reverse-proxy-0.5.2-dev" = "sha256-R1ZXGgWvwHWRHmKX823QLqM6ZJW+tzWUXigKkAyI5OE=";
+             "permit-client-rs-2.0.0" = "sha256-MxsgqPbvWDYDOb3oGuD1I6d3cdcGAhfoWsI7cwfhrb4=";
+             "permit-pdp-client-rs-0.2.0" = "sha256-F9wSvo3WzoRXjZb+We0Bvcwx3rRSG1QxXPsvrmtIN38=";
+             };
+          };
+          # cargoHash = "";
+        };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            nixpkgs-fmt
+            openssl
+            pkg-config
+            rust-bin.beta.latest.default
+            sqlx-cli
+            shuttle
+          ];
+
+          # shellHook = ''
+          #   alias ls=eza
+          #   alias find=fd
+          # '';
+        };
+      }
+    );
+}
