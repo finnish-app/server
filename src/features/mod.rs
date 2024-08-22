86e25fe0c9e2e76d0f97627f6fe768bdd11d2dd9
Nicolas Auler <nicolasauler@usp.br>
2024-08-22 00:58:03 -0300

    feat(db): query that inserts an item into pluggy_items table

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,1 +1,2 @@
+pub mod openfinance;
 pub mod totp;
---
+++ src/features/openfinance.rs
@@ -0,0 +1,20 @@
+use chrono::{DateTime, Utc};
+use sqlx::{postgres::PgQueryResult, Error, Pool, Postgres};
+use uuid::Uuid;
+
+pub async fn add_item(
+    db_pool: &Pool<Postgres>,
+    user_id: i32,
+    item_id: Uuid,
+    now: DateTime<Utc>,
+) -> Result<PgQueryResult, Error> {
+    sqlx::query!(
+        r#"INSERT INTO pluggy_items (user_id, external_item_id, created_at)
+    VALUES ($1, $2, $3)"#,
+        user_id,
+        item_id,
+        now
+    )
+    .execute(db_pool)
+    .await
+}
