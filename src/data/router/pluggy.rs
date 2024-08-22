ace4f6caac2ef61831c0fbedbc76cc661abde23e
Nicolas Auler <nicolasauler@usp.br>
2024-08-22 00:56:02 -0300

    feat: send item_id back to server after pluggy connect widget connects successfully

--- src/data/router/pluggy.rs
+++ src/data/router/pluggy.rs
@@ -3,14 +3,50 @@ use std::sync::Arc;
 use crate::client::pluggy::auth::ApiKey;
 use axum::{response::Json, routing::post, Router};
 use serde::{Deserialize, Serialize};
-// use uuid::Uuid;
+use uuid::Uuid;
 
 use crate::{auth::AuthSession, client::pluggy::account::ListAccountsResponse, AppState};
 
 pub fn router() -> Router<Arc<AppState>> {
     Router::new()
+        .route("/api/pluggyconnect/success", post(connect_success))
         .route("/api/accounts", post(list_accounts))
         .route("/api/transactions", post(list_transactions))
+}
+
+#[derive(Deserialize, Serialize)]
+struct ConnectRequest {
+    item_id: Uuid,
+}
+
+async fn connect_success(
+    auth_session: AuthSession,
+    State(shared_state): State<Arc<AppState>>,
+    Json(pluggyconnect_request): Json<ConnectRequest>,
+) -> StatusCode {
+    let Some(user) = auth_session.user else {
+        panic!("user not logged in")
+    };
+    tracing::debug!("User logged in");
+
+    match crate::features::openfinance::add_item(
+        &shared_state.pool,
+        user.id,
+        pluggyconnect_request.item_id,
+        chrono::Utc::now(),
+    )
+    .await
+    {
+        Ok(r) => {
+            assert!(r.rows_affected() < 2);
+            StatusCode::CREATED
+        }
+
+        Err(e) => {
+            tracing::error!(?e, user.id, "error receiving pluggy connect item_id");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize)]
