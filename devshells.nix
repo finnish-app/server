ed0e34dc2246074392457291d7754faabb136b6c
Nícolas Varela Auler <nicolasauler@usp.br>
2024-11-29 09:57:36 -0300

    ci: attempt to run checks at buildbot-nix (#159)
    * ci: attempting build and clippy on buildbot-nix
    
    * unrelated: removing unneded cargo audit bypass
    
    * bump: updates to flake and cargo to trigger rebuild

---
+++ devshells.nix
@@ -0,0 +1,31 @@
+{
+  perSystem = {
+    inputs',
+    self',
+    pkgs,
+    system,
+    ...
+  }: {
+    devShells.default = let
+      rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+    in
+      pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          bacon
+          cargo-expand
+          cargo-llvm-cov
+          cargo-nextest
+          cargo-watch
+          jq
+          nixpkgs-fmt
+          openssl
+          pkg-config
+          postgresql
+          python3
+          rust
+          svix-cli
+          sqlx-cli
+        ];
+      };
+  };
+}
