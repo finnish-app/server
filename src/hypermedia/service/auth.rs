0f12cf928d02dc84677795e6abd6f27490ba7774
Nicolas Auler <nicolasauler@usp.br>
2024-12-02 23:43:31 -0300

    refa: renaming finnish to fina in a couple of places

--- src/hypermedia/service/auth.rs
+++ src/hypermedia/service/auth.rs
@@ -134,7 +134,7 @@ pub async fn mfa_verify(
         1,
         30,
         secret.to_bytes().unwrap(),
-        Some("Finnish".to_owned()),
+        Some("Fina".to_owned()),
         user.email,
     )
     .unwrap();
